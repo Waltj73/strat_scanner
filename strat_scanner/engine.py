790b85269f7e0a294628afc15f8bed8c1e8c8127
Waltj73 <walt.seas@gmail.com>
2026-02-03 16:32:28 -0600

    Update engine.py

--- strat_scanner/engine.py
+++ strat_scanner/engine.py
@@ -50,7 +50,8 @@ def analyze_ticker(
 
     # --- REAL Strat trigger ---
     direction = "LONG" if trend == "UP" else "SHORT"
-    trigger = best_trigger(close)
+    trigger = best_trigger(df)
+
 
 
     return {
