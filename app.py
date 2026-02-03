fba7d6746babce5e1b0e7e2efc02282153c666f2
Waltj73 <walt.seas@gmail.com>
2026-02-03 15:18:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,24 @@
 import streamlit as st
 
-# Import page renderers
 from strat_scanner.pages.scanner import show_scanner
 from strat_scanner.pages.dashboard import show_dashboard
 from strat_scanner.pages.analyzer import show_analyzer
 from strat_scanner.pages.guide import show_guide
 
-
-# ---------------------------
-# Page Config
-# ---------------------------
 st.set_page_config(
-    page_title="STRAT Regime Scanner",
-    layout="wide"
+    page_title="STRAT Scanner",
+    layout="wide",
 )
 
-st.title("STRAT Regime Scanner")
+PAGES = {
+    "Scanner": show_scanner,
+    "Market Dashboard": show_dashboard,
+    "Ticker Analyzer": show_analyzer,
+    "User Guide": show_guide,
+}
 
-# ---------------------------
-# Sidebar Navigation
-# ---------------------------
-st.sidebar.title("Navigation")
+with st.sidebar:
+    st.title("Navigation")
+    choice = st.radio("Go to", list(PAGES.keys()), index=0)
 
-page = st.sidebar.radio(
-    "Go to",
-    [
-        "Scanner",
-        "Market Dashboard",
-        "Ticker Analyzer",
-        "User Guide"
-    ]
-)
-
-# ---------------------------
-# Page Routing
-# ---------------------------
-if page == "Scanner":
-    show_scanner()
-
-elif page == "Market Dashboard":
-    show_dashboard()
-
-elif page == "Ticker Analyzer":
-    show_analyzer()
-
-else:
-    show_guide()
-
+PAGES[choice]()
