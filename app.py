d9d7137d63265848bd72e9713027e7ca1b1328e1
Waltj73 <walt.seas@gmail.com>
2026-02-03 18:43:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 import streamlit as st
 
 from strat_scanner.pages.scanner import show_scanner
@@ -6,15 +5,25 @@ from strat_scanner.pages.dashboard import show_dashboard
 from strat_scanner.pages.analyzer import show_analyzer
 from strat_scanner.pages.guide import show_guide
 
-st.set_page_config(page_title="STRAT Scanner", layout="wide")
+
+st.set_page_config(
+    page_title="STRAT Scanner",
+    page_icon="📌",
+    layout="wide"
+)
+
+st.sidebar.title("Navigation")
+choice = st.sidebar.radio(
+    "Go to",
+    ["Scanner", "Market Dashboard", "Ticker Analyzer", "User Guide"],
+    index=0
+)
 
 PAGES = {
     "Scanner": show_scanner,
-    "Dashboard": show_dashboard,
+    "Market Dashboard": show_dashboard,
     "Ticker Analyzer": show_analyzer,
-    "Guide": show_guide,
+    "User Guide": show_guide,
 }
 
-st.sidebar.title("Navigation")
-choice = st.sidebar.radio("Go to", list(PAGES.keys()))
 PAGES[choice]()
