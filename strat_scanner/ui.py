7cb884b96691b7df44b4910f459b8c5a19a4665f
Waltj73 <walt.seas@gmail.com>
2026-02-03 19:25:15 -0600

    Delete strat_scanner directory

--- strat_scanner/ui.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-def meter_style(val: str) -> str:
-    if val == "STRONG":
-        return "background-color: #114b2b; color: white;"
-    if val == "NEUTRAL":
-        return "background-color: #5a4b11; color: white;"
-    return "background-color: #5a1111; color: white;"
